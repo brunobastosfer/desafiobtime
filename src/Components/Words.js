431f23046f23600f9fb5d1a9205a93ef8206412d
brunobastosfer <brunobastos@edu.unifor.br>
2021-09-07 23:50:50 -0300

    removendo os nomes repetidos e iniciando o componente Words

--- src/Components/Words.js
+++ src/Components/Words.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home  = ({words}) => {
-  console.log(words)
+  const removeRepeats = (array) => {
+    const countries = array.map((item) => item.map((item) => item.country))
+    const removeRepeats = countries.map((item) => item[0])
+    return removeRepeats
+  }
+  removeRepeats(words)
   return (
     <div>
       {/* <h1>{item.country}</h1> */}
