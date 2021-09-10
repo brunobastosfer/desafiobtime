007dddab58be34a6f9785746da57129bff427622
brunobastosfer <brunobastos@edu.unifor.br>
2021-09-09 23:04:38 -0300

    criando funções para filtrar, mapear e gerar um novo objeto

--- src/Components/Words.js
+++ src/Components/Words.js
@@ -1,17 +1,38 @@
 import React from "react";
 
-const Home  = ({words}) => {
+const Words  = ({words}) => {
+
   const removeRepeats = (array) => {
     const countries = array.map((item) => item.map((item) => item.country))
     const removeRepeats = countries.map((item) => item[0])
     return removeRepeats
   }
   removeRepeats(words)
+
+  const medalCount = (array, medal) => {
+    return array.filter((item) => item.medal === medal)
+  }
+
+  const objectGeneration = (words, removeRepeats) => {
+    return words.map((item, index) => {
+      return {
+        country: removeRepeats[index],
+        total: item.length,
+        position: index+1,
+        bronzeMedal: medalCount(item, 'Bronze'),
+        silverMedal: medalCount(item, 'Silver'),
+        goldMedal: medalCount(item, 'Gold'),
+      }
+    })
+  }
+
+  objectGeneration(words, removeRepeats(words))
+
   return (
     <div>
-      {/* <h1>{item.country}</h1> */}
+      {/* {words && listCountries(words, removeRepeats(words))} */}
     </div>
   )
 }
 
-export default Home;
+export default Words;
